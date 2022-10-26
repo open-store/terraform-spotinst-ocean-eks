acde9b94b891400ccc67d5d87eab408678cd10c1
Ari Falkner <33237990+arifalkner@users.noreply.github.com>
2022-10-26 11:33:10 -0400

    remove aws constraint

--- versions.tf
+++ versions.tf
@@ -10,10 +10,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.37"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1"
